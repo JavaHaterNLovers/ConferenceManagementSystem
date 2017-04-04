920051a69d234fc05bc2c81119d5691291033f78
Toader Mihai <toadermihai16@gmail.com>
2017-04-04 19:16:17 +0300

    Add registration

--- src/main/java/controller/RegisterCtrl.java
+++ src/main/java/controller/RegisterCtrl.java
@@ -1,11 +1,14 @@
 package controller;
 
+import domain.User;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 import service.UserService;
 import util.Controller;
+import util.UIUtil;
 
 
 /**
@@ -34,8 +37,78 @@ public class RegisterCtrl extends Controller {
         this.service = (UserService) this.get("service.user");
     }
 
+
+    /**
+     * @return true if password is valid, false otherwise
+     * Check if password length is bigger then 5 characters
+     * Check if password field and repeat password field contain the same value
+     */
+    private Boolean validatePasswords(){
+        if (passwordTxt.getText().length() < 5){
+            UIUtil.errorAlert("Password should be at least 5 characters.");
+            return false;
+        }
+        if (!passwordTxt.getText().equals(repeatPassTxt.getText())){
+            UIUtil.errorAlert("Passwords don't match!");
+            return false;
+        }
+        return true;
+    }
+
+
+    /**
+     * @return true if textFields are not empty, false otherwise
+     * Check for each textField if it is empty
+     */
+    private Boolean notEmptyFields(){
+        String errors = "";
+        if (firstNameTxt.getText().equals("")){
+            errors += "First name can't be empty!\n";
+        }
+        if (secondNameTxt.getText().equals("")){
+            errors += "Second name can't be empty!\n";
+        }
+        if (emailTxt.getText().equals("")){
+            errors += "E-mail can't be empty!\n";
+        }
+        if (passwordTxt.getText().equals("")){
+            errors += "Password field can't be empty!\n";
+        }
+        if (errors.equals("")) return true;
+        UIUtil.errorAlert(errors);
+        return false;
+    }
+
+    /**
+     * Clear password fields
+     */
+    private void clearPasswordFields(){
+        passwordTxt.clear();
+        repeatPassTxt.clear();
+    }
+
+    /**
+     * @param event ActionEvent
+     * Register a new user
+     */
     @FXML
     void registerAction(ActionEvent event) {
+        if (notEmptyFields() && validatePasswords()){
+            User user = service.validUser(emailTxt.getText(), passwordTxt.getText());
+            if (user != null) {
+                UIUtil.errorAlert("An user with this e-mail is already registered.");
+            }else{
+                try {
+                    service.add(emailTxt.getText(), passwordTxt.getText(), firstNameTxt.getText(), secondNameTxt.getText(), null);
+                    ((Stage) emailTxt.getScene().getWindow()).close();
+                    UIUtil.showMessage("Registration", "Registration successful.\n\nPlease login to continue.");
+                    return;
+                }catch(RuntimeException re){
+                    UIUtil.errorAlert("Please provide a valid e-mail address.");
+                }
+            }
+        }
+        clearPasswordFields();
 
     }
 }
