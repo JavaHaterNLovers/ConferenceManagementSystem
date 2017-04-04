7de430302e6d0e3f8b1911507f4f767edd4416b1
Clusi <sebestinclaudia@gmail.com>
2017-04-04 13:49:45 +0300

    Add Section entity and validation

--- src/main/java/domain/Section.java
+++ src/main/java/domain/Section.java
@@ -1,24 +1,75 @@
 package domain;
 
-/**
- *
- */
+import org.hibernate.validator.constraints.NotBlank;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="section")
 public class Section
 {
-    public Section() {}
 
-    /**
-     *
-     */
-    private int id;
 
-    /**
-     *
-     */
+    @Column
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    @Column
+    @NotBlank(message="{section.name}")
     private String name;
 
-    /**
-     *
-     */
+    @Column
+    @NotBlank(message = "{section.room}")
     private String room;
+
+    public Section() {this(null,null);}
+
+    public Section( String name, String room) {
+        this.id = null;
+        this.name = name;
+        this.room = room;
+    }
+    /**
+     * Returneaza id-ul.
+     *
+     * @return
+     */
+
+    public int getId() {
+        return id;
+    }
+
+
+    /**
+     * Returneaza numele
+     * @return
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Seteaza numele
+     * @param name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Returneaza camera
+     * @return
+     */
+    public String getRoom() {
+        return room;
+    }
+
+    /**
+     * Seteaza camera
+     * @param room
+     */
+    public void setRoom(String room) {
+        this.room = room;
+    }
 }
