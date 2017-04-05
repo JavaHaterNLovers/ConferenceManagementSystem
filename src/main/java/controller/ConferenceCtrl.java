87880d8cab738dbb84d49005ca555a3bd874a021
icoroian <coroian.iulia@gmail.com>
2017-04-05 18:48:02 +0300

    Add ConferenceForm fxml

--- src/main/java/controller/ConferenceCtrl.java
+++ src/main/java/controller/ConferenceCtrl.java
@@ -1,10 +1,13 @@
 package controller;
 
 import domain.Conference;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import service.ConferenceService;
+import util.BaseModel;
 import util.Controller;
 
 import java.util.List;
@@ -16,20 +19,17 @@ public class ConferenceCtrl extends Controller {
 
     private ConferenceService service;
 
-    private TableView<Conference> conferenceTable;
-
     @FXML
-    private void initialize() {
-        this.service = (ConferenceService) this.get("service.Conference");
-    }
+    private TableView<Conference> tableConference;
 
-    /**
-     * Get all conferences
-     * @return
-     */
-    public List<Conference> GetAll()
-    {
-        return service.all();
+    private BaseModel<Conference> model;
+
+    private ConferenceCtrl() {
+
+        this.service = (ConferenceService) this.get("service.Conference");
+        model = new BaseModel<>(service.all());
+
+        initData();
     }
 
     /**
@@ -37,21 +37,9 @@ public class ConferenceCtrl extends Controller {
      */
     public void initData()
     {
-        TableColumn authorCol = new TableColumn("Author");
-        TableColumn nameCol = new TableColumn("Name");
-        TableColumn beginSubmissionsCol = new TableColumn("Begin Submissions");
-        TableColumn endSubmissionsCol = new TableColumn("End Submissions");
-        TableColumn endBiddingCol = new TableColumn("End Bidding");
-        TableColumn endReviewCol = new TableColumn("End Review");
-        TableColumn createdCol = new TableColumn("Created");
-
-        conferenceTable.getColumns().addAll(authorCol, nameCol, beginSubmissionsCol, endSubmissionsCol,
-                endBiddingCol, endReviewCol, createdCol);
-
-        conferenceTable.getItems().clear();
-        for(Conference c:service.all())
-        {
-            conferenceTable.getItems().add(c);
+        tableConference.getItems().clear();
+        for (Conference conf:model.getList()) {
+            tableConference.getItems().add(conf);
         }
     }
 }
