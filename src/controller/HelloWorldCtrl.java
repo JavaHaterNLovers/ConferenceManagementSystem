3351857cc204b8949b8910a4e8b7366c741ca514
Rares Serban <rares_serban96@yahoo.com>
2017-04-16 12:17:30 +0300

    Add basic web app with spring security.

---
+++ src/controller/HelloWorldCtrl.java
@@ -0,0 +1,21 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloWorldCtrl
+{
+    @RequestMapping("/")
+    public ModelAndView index() {
+        return new ModelAndView("index");
+    }
+
+    @RequestMapping("/welcome")
+    public ModelAndView helloWorld() {
+        String message = "<br><div style='text-align:center;'>"
+                + "<h3>********** Hello World, Spring MVC Tutorial</h3>This message is coming from CrunchifyHelloWorld.java **********</div><br><br>";
+        return new ModelAndView("welcome", "message", message);
+    }
+}
